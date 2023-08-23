883d72dd14137e481d3c6977a1e8e61496ae1966
Rajkumar <rajkumarpatel52247@gmail.com>
2023-08-23 11:23:11 +0530

    s

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "component" {}
 variable "env" {}
 variable "engine" {}
-variable "engine version" {}
+variable "engine_version" {}
 variable "db_name" {}
 variable "subnet_ids" {}
 variable "tags" {}
